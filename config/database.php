266630116c73f6b578385306e5e3dd2ab801669f
ratataque <ewanrimmely@outlook.fr>
2025-11-06 23:50:06 +0100

    fix db

--- config/database.php
+++ config/database.php
@@ -58,10 +58,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', false),
-            ]) : [],
+            'options' => [],
         ],
 
         'pgsql' => [
